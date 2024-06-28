465c2defbeca6a5e8ac97a115f7fc4205508649d
Maarten de Keizer <github@maartendekeizer.nl>
2024-06-28 15:14:05 +0200

    Add check for http responses

--- client-controller/src/init.php
+++ client-controller/src/init.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
 
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
@@ -12,27 +13,32 @@ echo 'Gazeto init client' . PHP_EOL;
 
 $deviceToken = '';
 if (file_exists('/boot/gazeto-device-token.txt') === true) {
-    $deviceToken = file_get_contents('/boot/gazeto-device-token.txt');
+    $deviceToken = trim(file_get_contents('/boot/gazeto-device-token.txt'));
     echo 'Device token: ' . $deviceToken . PHP_EOL;
 } else {
     echo 'Device token not set' . PHP_EOL;
-    exit(-100);
+    exit(100);
 }
 
 $authToken = '';
 $exchangeToken = '';
 if (file_exists('/boot/gazeto-exchange-token.txt') === true && file_exists('/boot/gazeto-auth-token.txt') === true) {
-    $authToken = file_get_contents('/boot/gazeto-auth-token.txt');
-    $exchangeToken = file_get_contents('/boot/gazeto-exchange-token.txt');
+    $authToken = trim(file_get_contents('/boot/gazeto-auth-token.txt'));
+    $exchangeToken = trim(file_get_contents('/boot/gazeto-exchange-token.txt'));
     echo 'Auth token already set: ' . $authToken . PHP_EOL;
     echo 'Exchange token already set: ' . $exchangeToken . PHP_EOL;
 } else {
-    $inviteResponse = $client->request('POST', 'https://www.markeigazeto.nl/api/invite', [
-        'body' => ['deviceToken' => $deviceToken]
-    ])->toArray();
-    if ($inviteResponse['code'] !== 'INVITE_CREATED') {
-        echo 'Invite declined' . PHP_EOL;
-        exit(-101);
+    try {
+        $inviteResponse = $client->request('POST', 'https://www.markeigazeto.nl/api/invite', [
+            'body' => ['deviceToken' => $deviceToken]
+        ])->toArray();
+        if ($inviteResponse['code'] !== 'INVITE_CREATED') {
+            echo 'Invite declined' . PHP_EOL;
+            exit(101);
+        }
+    } catch (HttpExceptionInterface $e) {
+        echo 'Could not make contact with Gazeto server, code=' . $e->getResponse()->getStatusCode() . ' body=' . $e->getResponse()->getContent(false) . PHP_EOL;
+        exit(102);
     }
     $authToken = $inviteResponse['authToken'];
     $exchangeToken = $inviteResponse['exchangeToken'];
@@ -44,7 +50,7 @@ if (file_exists('/boot/gazeto-exchange-token.txt') === true && file_exists('/boo
 
 $displayToken = '';
 if (file_exists('/boot/gazeto-display-token.txt') === true) {
-    $displayToken = file_get_contents('/boot/gazeto-display-token.txt');
+    $displayToken = trim(file_get_contents('/boot/gazeto-display-token.txt'));
     echo 'Display token: ' . $displayToken . PHP_EOL;
 } else {
     while(true) {
