1150c483a2bc599b7526288571add096423afceb
Maarten de Keizer <github@maartendekeizer.nl>
2024-09-28 16:20:23 +0200

    Split functions and worker Add report command and hourly report Add Firefox policy for auto play

---
+++ client-controller/src/functions.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+function reboot(): string {
+    $process = new Process(['systemctl', 'reboot']);
+    $process->run();
+    return $process->getOutput();
+}
+
+function report(): string {
+    global $client, $urls;
+    assert($client instanceof HttpClientInterface);
+
+    $commands = [
+        'linux_distribution_description' => ['cmd' => ['lsb_release', '-d'], 'wd' => '/'],
+        'linux_distribution_version' => ['cmd' => ['lsb_release', '-r'], 'wd' => '/'],
+        'linux_distribution_codename' => ['cmd' => ['lsb_release', '-c'], 'wd' => '/'],
+        'debian_version' => ['cmd' => ['cat', '/etc/debian_version'], 'wd' => '/'],
+        'rpi_issue' => ['cmd' => ['cat', '/boot/issue.txt'], 'wd' => '/'],
+        'rpi_config' => ['cmd' => ['cat', '/boot/firmware/config.txt'], 'wd' => '/'],
+        'resolv_conf' => ['cmd' => ['cat', '/etc/resolv.conf'], 'wd' => '/'],
+        'date' => ['cmd' => ['date'], 'wd' => '/'],
+        'ip' => ['cmd' => ['ip', 'a'], 'wd' => '/'],
+        'cpu_info' => ['cmd' => ['cat', '/proc/cpuinfo'], 'wd' => '/'],
+        'gazeto_client' => ['cmd' => ['git', 'rev-parse', 'HEAD'], 'wd' => '/opt/gazeto-client']
+    ];
+
+    $output = [];
+    foreach ($commands as $k => $command) {
+        $process = new Process($command['cmd']);
+        $process->setWorkingDirectory($command['wd']);
+        $process->run();
+        $output[$k] = $process->getOutput();
+    }
+
+    $response = $client->request('POST', $urls['report'], [
+        'json' => $output
+    ]);
+    return $response->getContent(false);
+}
+
+function packageUpdate(): string {
+    $process = new Process(['apt', 'update']);
+    $process->run();
+    $outputBuffer = $process->getOutput();
+
+    $process = new Process(['apt', 'upgrade', '-y']);
+    $process->run();
+    return $outputBuffer . "\r\n" . $process->getOutput();
+}
+
+function gazetoUpdate(): string {
+    $process = new Process(['git', 'pull']);
+    $process->setWorkingDirectory('/opt/gazeto-client');
+    $process->run();
+    $outputBuffer = $process->getOutput();
+
+    $process = new Process(['ansible-playbook', 'playbook.yml']);
+    $process->setWorkingDirectory('/opt/gazeto-client');
+    $process->run();
+
+    return $outputBuffer . "\r\n" . $process->getOutput();
+}
--- client-controller/src/worker.php
+++ client-controller/src/worker.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Component\Process\Process;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+include __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
 
 sleep(60);
 
@@ -49,65 +48,4 @@ while(true) {
     }
 
     sleep(60);
-}
-
-function reboot(): string {
-    $process = new Process(['systemctl', 'reboot']);
-    $process->run();
-    return $process->getOutput();
-}
-
-function report(): string {
-    global $client, $urls;
-    assert($client instanceof HttpClientInterface);
-
-    $commands = [
-        'linux_distribution_description' => ['cmd' => ['lsb_release', '-d'], 'wd' => '/'],
-        'linux_distribution_version' => ['cmd' => ['lsb_release', '-r'], 'wd' => '/'],
-        'linux_distribution_codename' => ['cmd' => ['lsb_release', '-c'], 'wd' => '/'],
-        'debian_version' => ['cmd' => ['cat', '/etc/debian_version'], 'wd' => '/'],
-        'rpi_issue' => ['cmd' => ['cat', '/boot/issue.txt'], 'wd' => '/'],
-        'rpi_config' => ['cmd' => ['cat', '/boot/firmware/config.txt'], 'wd' => '/'],
-        'resolv_conf' => ['cmd' => ['cat', '/etc/resolv.conf'], 'wd' => '/'],
-        'date' => ['cmd' => ['date'], 'wd' => '/'],
-        'ip' => ['cmd' => ['ip', 'a'], 'wd' => '/'],
-        'cpu_info' => ['cmd' => ['cat', '/proc/cpuinfo'], 'wd' => '/'],
-        'gazeto_client' => ['cmd' => ['git', 'rev-parse', 'HEAD'], 'wd' => '/opt/gazeto-client']
-    ];
-
-    $output = [];
-    foreach ($commands as $k => $command) {
-        $process = new Process($command['cmd']);
-        $process->setWorkingDirectory($command['wd']);
-        $process->run();
-        $output[$k] = $process->getOutput();
-    }
-
-    $response = $client->request('POST', $urls['report'], [
-        'json' => $output
-    ]);
-    return $response->getContent(false);
-}
-
-function packageUpdate(): string {
-    $process = new Process(['apt', 'update']);
-    $process->run();
-    $outputBuffer = $process->getOutput();
-
-    $process = new Process(['apt', 'upgrade', '-y']);
-    $process->run();
-    return $outputBuffer . "\r\n" . $process->getOutput();
-}
-
-function gazetoUpdate(): string {
-    $process = new Process(['git', 'pull']);
-    $process->setWorkingDirectory('/opt/gazeto-client');
-    $process->run();
-    $outputBuffer = $process->getOutput();
-
-    $process = new Process(['ansible-playbook', 'playbook.yml']);
-    $process->setWorkingDirectory('/opt/gazeto-client');
-    $process->run();
-
-    return $outputBuffer . "\r\n" . $process->getOutput();
 }
