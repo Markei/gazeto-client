3eec2d06d9e68e7fb0d2cbbebe2d1d76b2fb1bd8
Maarten de Keizer <github@maartendekeizer.nl>
2025-01-07 12:57:52 +0100

    Auto restart Nginx and delay Firefox start untill Nginx is started

--- files/start.sh
+++ files/start.sh
@@ -3,7 +3,14 @@
 set -x
 set -e
 
-sleep 5
+# wait until nginx is started
+while ! netstat -tna | grep 'LISTEN\>' | grep -q ':80\>'; do
+  sleep 10
+done
+
+sleep 10
+
 #chromium-browser --kiosk http://localhost/client
 #firefox --display=:0 --kiosk-monitor 0 --private-window --kiosk http://localhost/client
+
 MOZ_ENABLE_WAYLAND=1 firefox --kiosk-monitor 0 --display=:0 --private-window --kiosk http://localhost/client
